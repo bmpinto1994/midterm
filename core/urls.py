1367d87ac5f18b0554e2b293866f9f02bd737603
Brian <Brian.Pinto@quinnipiac.edu>
2015-10-20 22:05:50 +0000

    Add message create view

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from .views import *
 
 urlpatterns = patterns('',
-                      url(r'^&', Home.as_view(), name='home'),
+                      url(r'^$', Home.as_view(), name='home'),
+                       url(r'^message/create/$', MessageCreateView.as_view(), name='message_create'),
                       )
--- core/views.py
+++ core/views.py
@@ -3,4 +3,14 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
   template_name = "home.html"
+
+from django.views.generic import CreateView
+from django.core.urlresolvers import reverse_lazy
+from .models import *
+
+class MessageCreateView(CreateView):
+  model = Message
+  template_name = "message/message_form.html"
+  fields = ['title', 'description']
+  success_url = reverse_lazy('home')
 # Create your views here.
