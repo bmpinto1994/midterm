a4b250f986e7d3ae702d132fb56f277dca2f5359
Brian <Brian.Pinto@quinnipiac.edu>
2015-10-20 22:13:31 +0000

    Add messages to admin panel

--- core/models.py
+++ core/models.py
@@ -5,4 +5,7 @@ class Message(models.Model):
   description = models.TextField(null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
 
+  def __unicode__(self):
+    return self.title
+
 # Create your models here.
