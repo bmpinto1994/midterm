a2bc25c9d2a0cc6a4f19f24868b4468e8bdf6aac
Brian <Brian.Pinto@quinnipiac.edu>
2015-10-22 18:15:12 +0000

    Fix errors

---
+++ core/migrations/0002_auto_20151022_1813.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='message',
+            old_name='title',
+            new_name='email',
+        ),
+        migrations.RenameField(
+            model_name='message',
+            old_name='description',
+            new_name='message',
+        ),
+        migrations.AddField(
+            model_name='message',
+            name='name',
+            field=models.CharField(default='test', max_length=300),
+            preserve_default=False,
+        ),
+    ]
